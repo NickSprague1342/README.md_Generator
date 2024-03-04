bc1be29998badd1bcebc82b0e4a0c3b8707a6a61
Nick Sprague <n.sprague18@gmail.com>
2024-03-03 22:59:58 -0500

    working on adding inquirer questions

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,37 @@
 // TODO: Include packages needed for this application
 console.log('Testing node.js')
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+const genMarkdown = require('/Users/nicholassprague/bootcamp/CHALLENGES/README.md_Generator/utils/index.js')
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter project title.'
+    },
+
+    {
+        type: 'input',
+        title: 'description',
+        message: 'Please provide a project description.'
+    },
+
+    {
+        type: 'input',
+        title: 'contents',
+        message: 'Please provide a table of contents.'
+    },
+
+    {
+        type: 'input',
+        title: 'install',
+        message: 'Enter install instructions.',
+        default: 'Clone repo onto your machine. Install node.js. Run npm install to install dependencies to machine.'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
