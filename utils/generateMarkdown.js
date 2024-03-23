b265546f95742d6bdf97c0950c6b30b434e4956f
Nick Sprague <n.sprague18@gmail.com>
2024-03-23 19:43:29 -0400

    added conditional if statements in generate markdown.md for lisence

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+    return '[MIT](https://img.shields.io/badge/License-MIT-green)';
+  } if (license === 'GNU') {
+    return '[GNU](https://img.shields.io/badge/License-GNU-green)';
+  } if (license === 'BDS') {
+    return '[BDS](https://img.shields.io/badge/License-BDS-green)';
+  } if (license === 'Apache') {
+    return '[Apache](https://img.shields.io/badge/License-Apache-green)';
+  } if (license === 'Apache 2.0') {
+    return '[Apache 2.0](https://img.shields.io/badge/License-Apache-2.0-green)';
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
