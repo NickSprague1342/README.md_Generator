31d2a7862a2874205137ac8222cf996cde02ed1f
Nick Sprague <n.sprague18@gmail.com>
2024-03-23 20:04:14 -0400

    added conditionals for license link in generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -7,8 +7,6 @@ function renderLicenseBadge(license) {
     return '[GNU](https://img.shields.io/badge/License-GNU-green)';
   } if (license === 'BDS') {
     return '[BDS](https://img.shields.io/badge/License-BDS-green)';
-  } if (license === 'Apache') {
-    return '[Apache](https://img.shields.io/badge/License-Apache-green)';
   } if (license === 'Apache 2.0') {
     return '[Apache 2.0](https://img.shields.io/badge/License-Apache-2.0-green)';
   } else {
@@ -18,7 +16,19 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT](https://opensource.org/license/mit)';
+  } if (license === 'GNU') {
+    return '[GNU](https://www.gnu.org/licenses/gpl-3.0.en.html)';
+  } if (license === 'BDS') {
+    return '[BSD](https://opensource.org/license/bsd-3-clause)';
+  } if (license === 'Apache 2.0') {
+    return '[Apache 2.0](https://www.apache.org/licenses/LICENSE-2.0)';
+  } else {
+    return ' ';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
